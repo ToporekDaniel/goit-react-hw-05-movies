ba44439c68c1ecd1f3028d45bc327069bd78dd2c
ToporekDaniel <danieltoporek0@gmail.com>
2024-03-01 22:02:50 +0100

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './header/header';
+import { Home } from './home/home';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/home/home.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+      <h2>Trending today</h2>
+      <ul></ul>
+    </>
+  );
+};
